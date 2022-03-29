b916aca7a08e635639b8a9a0fe1282d2bd7a4c0b
Der_Googler <jxhazhungerz@gmail.com>
2022-03-29 23:34:55 +0200

    .

---
+++ app/src/main/cpp/exec.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jimmy on 29.03.22.
+//
+
+#include "exec.h"
---
+++ app/src/main/cpp/exec.h
@@ -0,0 +1,14 @@
+//
+// Created by jimmy on 29.03.22.
+//
+
+#ifndef FAKE_CHARGER_EXEC_H
+#define FAKE_CHARGER_EXEC_H
+
+
+class exec {
+
+};
+
+
+#endif //FAKE_CHARGER_EXEC_H
