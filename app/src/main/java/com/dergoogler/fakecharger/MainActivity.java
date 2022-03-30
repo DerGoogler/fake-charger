0c7506e2cf64bafdeb13d3f4d40996973bd2ed2a
Der_Googler <jxhazhungerz@gmail.com>
2022-03-30 13:32:33 +0200

    .

--- app/src/main/java/com/dergoogler/fakecharger/MainActivity.java
+++ app/src/main/java/com/dergoogler/fakecharger/MainActivity.java
@@ -1,18 +1,39 @@
 package com.dergoogler.fakecharger;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.topjohnwu.superuser.BusyBoxInstaller;
 import com.topjohnwu.superuser.Shell;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.io.InputStream;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    static {
+        Shell.enableVerboseLogging = BuildConfig.DEBUG;
+        Shell.setDefaultBuilder(Shell.Builder.create()
+                .setFlags(Shell.FLAG_REDIRECT_STDERR)
+                // BusyBoxInstaller should come first!
+                .setInitializers(BusyBoxInstaller.class, Initializer.class)
+        );
+    }
+
+    static class Initializer extends Shell.Initializer {
+        @Override
+        public boolean onInit(@NonNull Context context, @NonNull Shell shell) {
+            InputStream bashrc = context.getResources().openRawResource(R.raw.bashrc);
+            shell.newJob().add(bashrc).exec();
+            return true;
+        }
+    }
 
     @SuppressLint("SdCardPath")
     @Override
@@ -24,36 +45,24 @@ public class MainActivity extends AppCompatActivity {
         TextInputEditText maxSpeed = findViewById(R.id.maxSpeedEditText);
         TextInputEditText maxAdd = findViewById(R.id.maxAddEditText);
         Button startButton = findViewById(R.id.startButton);
-        TextInputEditText selfBattery = findViewById(R.id.selfMaxBattery);
-        Button setSelfBattery = findViewById(R.id.setSelfBattery);
-        Button resetSelfBattery = findViewById(R.id.resetSelfBattery);
+        //TextInputEditText selfBattery = findViewById(R.id.selfMaxBattery);
+        //Button setSelfBattery = findViewById(R.id.setSelfBattery);
+        //Button resetSelfBattery = findViewById(R.id.resetSelfBattery);
 
         startButton.setOnClickListener((view) -> {
-            final Shell.Result result;
-            String speed = "export _maxspeed_=" + getText(maxSpeed);
-            String add = "export _maxadd_=" + getText(maxAdd);
-            String battery = "export _maxcharge_=" + getText(maxBattery);
-
-            InputStream charger = this.getResources().openRawResource(R.raw.fakecharger);
-            result = Shell.cmd()
-                    .add(charger)
-                    .add(speed)
-                    .add(add)
-                    .add(battery)
-                    .exec();
-
-            if (result.isSuccess()) {
-                Shell.cmd("cmd battery reset");
-            }
+            String speed = "-s " + getText(maxSpeed) + " ";
+            String add = "-a " + getText(maxAdd) + " ";
+            String battery = "-m " + getText(maxBattery) + " ";
+            Shell.cmd("fakecharger " + speed + add + battery).exec();
         });
 
-        setSelfBattery.setOnClickListener((view) -> {
-            Shell.cmd("cmd battery set level " + getText(selfBattery)).exec();
-        });
+       // setSelfBattery.setOnClickListener((view) -> {
+        //    Shell.cmd("cmd battery set level " + getText(selfBattery)).exec();
+       // });
 
-        resetSelfBattery.setOnClickListener((view) -> {
-            Shell.cmd("cmd battery reset -f").exec();
-        });
+      //  resetSelfBattery.setOnClickListener((view) -> {
+        //    Shell.cmd("cmd battery reset -f").exec();
+        //});
 
     }
 
